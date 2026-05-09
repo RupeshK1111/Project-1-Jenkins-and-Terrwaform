c0594ce77d96b6d4872bf77e04d4faa8e7a58832
RupeshK1111 <rkorte8485@gmail.com>
2026-05-09 11:40:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-07ffb2f4d65357b42" 
+    default     = "ami-091138d0f0d41ff90" 
 }
 variable "versioning" {
     type        = bool
