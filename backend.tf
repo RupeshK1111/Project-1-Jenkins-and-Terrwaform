f1665849d61ef5c5147eaef5dc84ef1512a08c04
RupeshK1111 <rkorte8485@gmail.com>
2026-05-09 20:53:58 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state-123"
+    bucket = "young-minds-app-project-terraform-state-11"
     key = "main"
     region = "us-east-1"
     use_lockfile = true
